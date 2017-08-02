65c9e0e1d14c2a3c7ce4945fbb2a3111d13c59a9
Danny Tuppeny <danny@tuppeny.com>
2017-08-02 20:33:09 +0100

    Add common_types_spec.
    These used to be in spec_input but have now moved because they're shared with plugins.

--- bin/main.dart
+++ bin/main.dart
@@ -4,18 +4,32 @@ import 'dart:io';
 import 'package:analysis_server_typescript_generator/api_spec.dart';
 import 'package:analysis_server_typescript_generator/typescript_generator.dart';
 
-final _specUri = Uri.parse(
-    'https://github.com/dart-lang/sdk/raw/master/pkg/analysis_server/tool/spec/spec_input.html');
-final _file =
-    "/Users/danny/Dev/Google/sdk/pkg/analysis_server/tool/spec/spec_input.html";
-final _outputFile = new File("output/types.ts");
+final _specUris = [
+  Uri.parse(
+      'https://github.com/dart-lang/sdk/raw/master/pkg/analysis_server/tool/spec/spec_input.html'),
+  Uri.parse(
+      'https://github.com/dart-lang/sdk/raw/master/pkg/analyzer_plugin/tool/spec/common_types_spec.html')
+];
+final _files = [
+  "/Users/danny/Dev/Google/sdk/pkg/analysis_server/tool/spec/spec_input.html",
+  "/Users/danny/Dev/Google/sdk/pkg/analyzer_plugin/tool/spec/common_types_spec.html",
+];
+final _outputFile = new File("output/analysis_server_types.ts");
 
 Future main() async {
-  //final spec = await ApiSpec.download(_specUri);
-  final spec = ApiSpec.fromFile(_file);
   final gen = new TypeScriptGenerator();
 
-  spec.buildDefinitions(gen.definitions);
+  if (true) {
+    await Future.forEach(_specUris, (Uri specUri) async {
+      final spec = await ApiSpec.download(specUri);
+      spec.buildDefinitions(gen.definitions);
+    });
+  } else {
+    _files.forEach((file) {
+      final spec = ApiSpec.fromFile(file);
+      spec.buildDefinitions(gen.definitions);
+    });
+  }
 
   await gen.writeTo(_outputFile);
 }
