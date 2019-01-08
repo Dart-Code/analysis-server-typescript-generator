d56d392aaec5c1dceef469153010a9b8ddaf1d96
Danny Tuppeny <danny@tuppeny.com>
2019-01-08 15:40:58 +0000

    Fix all lints/warnings

--- lib/api_spec.dart
+++ lib/api_spec.dart
@@ -1,26 +1,59 @@
 import 'dart:async';
-
 import 'dart:io';
+
 import 'package:html/dom.dart';
 import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 import 'definitions.dart';
 
-class ApiSpec {
-  final Document _apiDoc;
+Element _getChild(Element element, [String tag]) =>
+    _getChildren(element, tag).firstWhere((e) => true, orElse: () => null);
 
+Iterable<Element> _getChildren(Element element, [String tag]) =>
+    element.children.where((c) =>
+        c.nodeType != Node.TEXT_NODE && (tag == null || c.localName == tag));
+
+String _getDocs(Element element) =>
+    _getChildren(element, 'p').map((p) => p.text.trim()).join("\r\n\r\n");
+
+String _getType(Element field) {
+  switch (field.localName) {
+    case 'ref':
+      return _getTypeScriptTypeName(field.text);
+    case 'list':
+      return '${_getType(_getChild(field))}[]';
+    case 'map':
+      return '{ [key: string]: ${_getType(_getChild(_getChild(field, "value")))}; }';
+    case 'union':
+      return _getChildren(field).map(_getType).join(" | ");
+    default:
+      throw new ArgumentError('Unknown ${field.parent.outerHtml}');
+  }
+}
+
+// Converts THIS_AND_THIS to ThisAndThis
+String _getTypeScriptTypeName(String dartType) {
+  const types = const {
+    "String": "string",
+    "int": "number",
+    "long": "number",
+    "bool": "boolean",
+  };
+
+  return types[dartType] ?? dartType;
+}
+
+String _prettyName(String str) =>
+    str.split('_').map((s) => _titleCase(s.toLowerCase())).join('');
+
+String _titleCase(String str) =>
+    str.substring(0, 1).toUpperCase() + str.substring(1);
+
+class ApiSpec {
   ApiSpec._(String spec) : _apiDoc = parse(spec);
 
-  static Future<ApiSpec> fromUri(Uri specUri) async {
-    final resp = await http.get(specUri);
-    return new ApiSpec._(resp.body);
-  }
-
-  static ApiSpec fromFile(String file) {
-    final body = new File(file).readAsStringSync();
-    return new ApiSpec._(body);
-  }
+  final Document _apiDoc;
 
   Iterable<Definition> getTypes() sync* {
     // Request/responses.
@@ -50,6 +83,38 @@ class ApiSpec {
     }).expand((x) => x);
   }
 
+  EnumDefinition _createEnum(Element type) {
+    final def = new EnumDefinition(type.attributes["name"], _getDocs(type));
+    def.values.addAll(_getChildren(_getChild(type, "enum"), "value")
+        .map((c) => _getChild(c, "code").text));
+    return def;
+  }
+
+  InterfaceDefinition _createInterface(Element method, String name, String type,
+      {bool allowEmpty: false, String parent}) {
+    final doc = _getDocs(method);
+    final properties = method.querySelectorAll("$type field");
+
+    if (properties.length == 0 && !allowEmpty) return null;
+
+    final def = new InterfaceDefinition(name, doc, parent: parent);
+    def.properties.addAll(properties.map(_getPropertyDefinition));
+    return def;
+  }
+
+  InterfaceDefinition _createNotificationInterface(Element event) =>
+      _createInterface(
+          event,
+          _titleCase(event.parent.attributes["name"]) +
+              _titleCase(event.attributes["event"]) +
+              "Notification",
+          "params");
+
+  InterfaceDefinition _createRefactoringFeedbackInterface(Element type) =>
+      _createInterface(
+          type, _prettyName(type.attributes["kind"]) + "Feedback", "feedback",
+          parent: "RefactoringFeedback");
+
   InterfaceDefinition _createRequestInterface(Element method) =>
       _createInterface(
           method,
@@ -66,46 +131,14 @@ class ApiSpec {
               "Response",
           "result");
 
-  InterfaceDefinition _createNotificationInterface(Element event) =>
-      _createInterface(
-          event,
-          _titleCase(event.parent.attributes["name"]) +
-              _titleCase(event.attributes["event"]) +
-              "Notification",
-          "params");
-
-  InterfaceDefinition _createTypeInterface(Element type) =>
-      _createInterface(type, _titleCase(type.attributes["name"]), "object",
-          allowEmpty: true);
-
-  EnumDefinition _createEnum(Element type) {
-    final def = new EnumDefinition(type.attributes["name"], _getDocs(type));
-    def.values.addAll(_getChildren(_getChild(type, "enum"), "value")
-        .map((c) => _getChild(c, "code").text));
-    return def;
-  }
-
   TypeAliasDefinition _createTypeAlias(Element type) {
     return new TypeAliasDefinition(_getType(_getChild(type, "ref")),
         type.attributes["name"], _getDocs(type));
   }
 
-  InterfaceDefinition _createRefactoringFeedbackInterface(Element type) =>
-      _createInterface(
-          type, _prettyName(type.attributes["kind"]) + "Feedback", "feedback",
-          parent: "RefactoringFeedback");
-
-  InterfaceDefinition _createInterface(Element method, String name, String type,
-      {bool allowEmpty: false, String parent}) {
-    final doc = _getDocs(method);
-    final properties = method.querySelectorAll("$type field");
-
-    if (properties.length == 0 && !allowEmpty) return null;
-
-    final def = new InterfaceDefinition(name, doc, parent: parent);
-    def.properties.addAll(properties.map(_getPropertyDefinition));
-    return def;
-  }
+  InterfaceDefinition _createTypeInterface(Element type) =>
+      _createInterface(type, _titleCase(type.attributes["name"]), "object",
+          allowEmpty: true);
 
   PropertyDefinition _getPropertyDefinition(Element field) {
     return new PropertyDefinition(
@@ -115,47 +148,14 @@ class ApiSpec {
         field.attributes["optional"] == "true",
         _getDocs(field));
   }
-}
 
-String _getTypeScriptTypeName(String dartType) {
-  const types = const {
-    "String": "string",
-    "int": "number",
-    "long": "number",
-    "bool": "boolean",
-  };
+  static ApiSpec fromFile(String file) {
+    final body = new File(file).readAsStringSync();
+    return new ApiSpec._(body);
+  }
 
-  return types[dartType] ?? dartType;
-}
-
-String _getType(Element field) {
-  switch (field.localName) {
-    case 'ref':
-      return _getTypeScriptTypeName(field.text);
-    case 'list':
-      return '${_getType(_getChild(field))}[]';
-    case 'map':
-      return '{ [key: string]: ${_getType(_getChild(_getChild(field, "value")))}; }';
-    case 'union':
-      return _getChildren(field).map(_getType).join(" | ");
-    default:
-      throw new ArgumentError('Unknown ${field.parent.outerHtml}');
+  static Future<ApiSpec> fromUri(Uri specUri) async {
+    final resp = await http.get(specUri);
+    return new ApiSpec._(resp.body);
   }
 }
-
-String _titleCase(String str) =>
-    str.substring(0, 1).toUpperCase() + str.substring(1);
-
-// Converts THIS_AND_THIS to ThisAndThis
-String _prettyName(String str) =>
-    str.split('_').map((s) => _titleCase(s.toLowerCase())).join('');
-
-String _getDocs(Element element) =>
-    _getChildren(element, 'p').map((p) => p.text.trim()).join("\r\n\r\n");
-
-Iterable<Element> _getChildren(Element element, [String tag]) =>
-    element.children.where((c) =>
-        c.nodeType != Node.TEXT_NODE && (tag == null || c.localName == tag));
-
-Element _getChild(Element element, [String tag]) =>
-    _getChildren(element, tag).firstWhere((e) => true, orElse: () => null);
--- lib/definitions.dart
+++ lib/definitions.dart
@@ -1,36 +1,36 @@
 class Definition {
+  Definition(this.name, this.doc);
+
   String name;
   String doc;
-
-  Definition(this.name, this.doc);
-}
-
-class InterfaceDefinition extends Definition {
-  String parent;
-  final properties = new List<PropertyDefinition>();
-
-  InterfaceDefinition(String name, String doc, {this.parent})
-      : super(name, doc);
-}
-
-class PropertyDefinition extends Definition {
-  String type;
-  String value;
-  bool isOptional;
-
-  PropertyDefinition(
-      this.type, this.value, String name, this.isOptional, String doc)
-      : super(name, doc);
 }
 
 class EnumDefinition extends Definition {
-  final values = new List<String>();
-
   EnumDefinition(String name, String doc) : super(name, doc);
+
+  final values = new List<String>();
+}
+
+class InterfaceDefinition extends Definition {
+  InterfaceDefinition(String name, String doc, {this.parent})
+      : super(name, doc);
+
+  String parent;
+  final properties = new List<PropertyDefinition>();
+}
+
+class PropertyDefinition extends Definition {
+  PropertyDefinition(
+      this.type, this.value, String name, this.isOptional, String doc)
+      : super(name, doc);
+
+  String type;
+  String value;
+  bool isOptional;
 }
 
 class TypeAliasDefinition extends Definition {
-  String type;
-
   TypeAliasDefinition(this.type, String name, String doc) : super(name, doc);
+
+  String type;
 }
